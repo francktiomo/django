f04bea38e9a687b55ec79158b104c2fc6352367c
Franck Tiomo <ef.tiomo@gmail.com>
2025-02-18 12:16:34 -0500

    Add models and relashionships between them

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import Author, Address, Book, Country
+
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ('slug',)
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('rating', 'author',)
+    list_display = ('title', 'author',)
+
+
+# class AuthorAdmin(admin.ModelAdmin):
+#     pass
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
