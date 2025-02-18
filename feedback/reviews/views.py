433d02107b4a462e94e77564bd4be6c403057d49
Franck Tiomo <ef.tiomo@gmail.com>
2025-02-18 14:46:40 -0500

    Limits of the form element

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+
+def review(request):
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+
+        if entered_username == '':
+            return render(request, 'reviews/review.html', {
+                'error': 'Please enter a username'
+            })
+        print(entered_username)
+        return HttpResponseRedirect('thx/')
+    return render(request, 'reviews/review.html', {
+        'error': ''
+    })
+
+
+def thx(request):
+    return render(request, 'reviews/thx.html')
