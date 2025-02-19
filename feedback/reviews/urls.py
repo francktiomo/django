5789312e62544828b284a08372ab0b7faf08d9dc
Franck Tiomo <ef.tiomo@gmail.com>
2025-02-19 13:42:09 -0500

    DetailView and ListView

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.ReviewView.as_view()),
     path('thx/', views.ThxView.as_view()),
     path('reviews/', views.ReviewsListView.as_view()),
-    path('reviews/<int:id>', views.ReviewDetailView.as_view())
+    path('reviews/<int:pk>', views.ReviewDetailView.as_view())
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -7,6 +7,8 @@ from .models import Review
 from django.http import HttpRequest
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 # Create your views here.
 
@@ -28,36 +30,49 @@ class ReviewView(View):
         return render(request, 'reviews/review.html', {
             'form': form
         })
-    
+
+
 class ThxView(TemplateView):
     template_name = 'reviews/thx.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['dynamic_content_example'] = 'Dynamic content from TemplateView'
         return context
-    
 
-class ReviewsListView(TemplateView):
+
+# class ReviewsListView(TemplateView):
+#     template_name = 'reviews/reviews.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         reviews = Review.objects.all()
+#         context['reviews'] = reviews
+#         return context
+
+class ReviewsListView(ListView):
     template_name = 'reviews/reviews.html'
+    model = Review
+    context_object_name = 'reviews'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context['reviews'] = reviews
-        return context
-    
-class ReviewDetailView(TemplateView):
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=4)
+    #     return data
+
+class ReviewDetailView(DetailView):
     template_name = 'reviews/review_detail.html'
+    model = Review # for which model i want a single instance for
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        id = kwargs['id']
-        review = Review.objects.get(pk=id)
-        context["review"] = review
-        return context
-    
-    
+# class ReviewDetailView(TemplateView):
+#     template_name = 'reviews/review_detail.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         id = kwargs['id']
+#         review = Review.objects.get(pk=id)
+#         context["review"] = review
+#         return context
 
 
 # def review(request: HttpRequest):
